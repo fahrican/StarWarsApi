505e56c5a635efd6d2a70fc2ea6f165f15738f4f
Fahri MacBook <fahri-can@web.de>
2019-12-23 02:36:33 +0100

    Start with ViewHolder and Adapter for species.

---
+++ app/src/main/java/com/example/starwarsapi/adapter/SpeciesViewHolderAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.starwarsapi.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class SpeciesAdapter() : RecyclerView.Adapter<SpeciesViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SpeciesViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: SpeciesViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+}
+
+class SpeciesViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+
+}
