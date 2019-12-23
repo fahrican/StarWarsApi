d2b11e8c0d1c8d215d5d0cf645d959ab6f0ff838
Fahri MacBook <fahri-can@web.de>
2019-12-23 20:18:02 +0100

    Finish Adapter for species.

--- app/src/main/java/com/example/starwarsapi/adapter/SpeciesViewHolderAdapter.kt
+++ app/src/main/java/com/example/starwarsapi/adapter/SpeciesViewHolderAdapter.kt
@@ -1,27 +1,55 @@
 package com.example.starwarsapi.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.starwarsapi.R
+import com.example.starwarsapi.model.Species
 import kotlinx.android.synthetic.main.item_species.view.*
 
-class SpeciesAdapter() : RecyclerView.Adapter<SpeciesViewHolder>() {
+class SpeciesAdapter(
+    private var speciesList: ArrayList<Species>
+) : RecyclerView.Adapter<SpeciesViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SpeciesViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val itemView: View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_species, parent, false)
+        return SpeciesViewHolder(itemView)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return speciesList.size
     }
 
     override fun onBindViewHolder(holder: SpeciesViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val species: Species = speciesList[position]
+        setSpeciesViewHolderProperties(holder, species)
+        holder.speciesCardView.setOnClickListener {
+            Toast.makeText(
+                holder.itemView.context,
+                "Name: ${holder.speciesName}",
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
 
+    private fun setSpeciesViewHolderProperties(
+        speciesViewHolder: SpeciesViewHolder,
+        species: Species
+    ) {
+        speciesViewHolder.speciesName.text = species.name
+        speciesViewHolder.speciesAVGLifespan.text = species.average_lifespan
+        speciesViewHolder.speciesLanguage.text = species.language
+    }
 
+    private fun setSpeciesList(listOfSpecies: ArrayList<Species>) {
+        speciesList = listOfSpecies
+        notifyDataSetChanged()
+    }
 }
 
 class SpeciesViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
