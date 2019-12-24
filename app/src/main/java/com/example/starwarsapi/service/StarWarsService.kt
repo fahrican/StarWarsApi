18c63b4b7e57f662b81058d8a98b466bc5e8c158
Fahri MacBook <fahri-can@web.de>
2019-12-24 18:04:07 +0100

    Implement service to load all species.

---
+++ app/src/main/java/com/example/starwarsapi/service/StarWarsService.kt
@@ -0,0 +1,27 @@
+package com.example.starwarsapi.service
+
+import com.example.starwarsapi.api_endpoint.StarWarsApi
+import com.example.starwarsapi.model.Species
+import io.reactivex.Single
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+class StarWarsService {
+
+    private val BASE_URL = "https://swapi.co/"
+    private val starWarsApi: StarWarsApi
+
+    init {
+        starWarsApi = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+            .create(StarWarsApi::class.java)
+    }
+
+    fun loadSpecies(): Single<List<Species>> {
+        return starWarsApi.getSpecies()
+    }
+}
