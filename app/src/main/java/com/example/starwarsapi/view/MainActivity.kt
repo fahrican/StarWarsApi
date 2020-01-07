0454af4bf4b008da307839568cd705799380b5da
Fahri MacBook <fahri-can@web.de>
2020-01-07 20:34:10 +0100

    Move all everything with LiveData observe in separate methods.

--- app/src/main/java/com/example/starwarsapi/view/MainActivity.kt
+++ app/src/main/java/com/example/starwarsapi/view/MainActivity.kt
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.starwarsapi.R
+import com.example.starwarsapi.model.Species
 import com.example.starwarsapi.viewmodel.SpeciesViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -26,7 +28,7 @@ class MainActivity : AppCompatActivity() {
 
         setUpViews()
 
-        observeLiveDataProperties()
+        observeLiveData()
     }
 
     private fun setUpViews() {
@@ -41,27 +43,40 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun observeLiveDataProperties() {
-        viewModel.speciesListMLD.observe(this, Observer { allSpecies ->
-            allSpecies.let {
-                main_recycler_view.visibility = View.VISIBLE
-                viewModel.speciesAdapter.setSpeciesList(it)
-            }
-        })
+    private fun observeLiveData() {
+        observeInProgress()
+        observeIsError()
+        observeSpeciesList()
+    }
 
-        viewModel.isErrorMLD.observe(this, Observer { isError ->
-            isError.let { species_list_error.visibility = if (it) View.VISIBLE else View.GONE }
-        })
-
-        viewModel.inProgressMLD.observe(this, Observer { isLoading ->
+    private fun observeInProgress() {
+        val inProgressLD: LiveData<Boolean> = viewModel.inProgressMLD
+        inProgressLD.observe(this, Observer { isLoading ->
             isLoading.let {
                 if (it) {
-                    species_list_progress.visibility = View.VISIBLE
                     species_list_error.visibility = View.GONE
                     main_recycler_view.visibility = View.GONE
+                    species_list_progress.visibility = View.VISIBLE
                 } else {
                     species_list_progress.visibility = View.GONE
                 }
+            }
+        })
+    }
+
+    private fun observeIsError() {
+        val isErrorLD: LiveData<Boolean> = viewModel.isErrorMLD
+        isErrorLD.observe(this, Observer { isError ->
+            isError.let { species_list_error.visibility = if (it) View.VISIBLE else View.GONE }
+        })
+    }
+
+    private fun observeSpeciesList() {
+        val speciesListLD: LiveData<List<Species>> = viewModel.speciesListMLD
+        speciesListLD.observe(this, Observer { allSpecies ->
+            allSpecies.let {
+                main_recycler_view.visibility = View.VISIBLE
+                viewModel.speciesAdapter.setSpeciesList(it)
             }
         })
     }
